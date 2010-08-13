b2c23cb0da99184c5d27c123394c4cc331921881
Rafael Bandeira <rafaelbandeira3@gmail.com>
2010-08-12 23:40:33 -0300

    railizificando o site, tudo funciona igual, mas o Rails que comanda agora

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9db6d6d402482615d17282caafab4ebeb670e82a3a5cc94d3f5267a300a0d1a245a2335de97ecb200aad2614811dd68f4672675fcb02c8f3d7b3792a74da65d2';
